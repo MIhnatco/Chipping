ce89f2632dc3ef5b579c9075f6be785ae5e999ed
Ihliana <Ihnatco@protonmail.com>
2023-09-25 15:44:10 +0200

    mobile first
    function for opening and closing side navigation for mobile devices.

---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bocnyPanel = document.getElementById("bocnaNavigacia")
+    const otvorBtn = document.getElementById("kruh")
+    const zatvorBtn = document.querySelector('.fa-close')
+
+    //funkcia pre otvorenie bocnu-navigaciu
+    function otvorNavigaciu() {
+        if (bocnyPanel.style.right === "-250px") {
+            bocnyPanel.style.right = "0px" // ukaz bocnu navigaciu
+            otvorBtn.style.display = "none"
+        }
+    }
+
+    //funkcia pre zavretie bocnu-navigaciu
+    function zavriNavigaciu() {
+        if (bocnyPanel.style.right === "0px") {
+            bocnyPanel.style.right = "-250px" // zavri bocnu navigaciu
+            otvorBtn.style.display = "flex"
+        }
+    }
+
+
+    //otvor event 
+    otvorBtn.addEventListener("click", otvorNavigaciu)
+
+    //zavri event
+    zatvorBtn.addEventListener("click", zavriNavigaciu)
+
+})
